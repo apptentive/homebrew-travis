fc2a0000ab5e2e2f06519302b04b080df498fee7
Frank Schmitt <frank@apptentive.com>
2020-09-18 16:27:33 -0700

    Fix directory structure

--- Library/Homebrew/test/os/os_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-describe OS do
-  describe ".kernel_version" do
-    it "is not empty" do
-      expect(described_class.kernel_version).not_to be_empty
-    end
-  end
-end
